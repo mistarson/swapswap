82d9de474c146d90e048b5975872068b33b5cd2e
손창현 (Son Chang Hyun) <cson90563@gmail.com>
2024-01-05 11:13:15 +0900

    Initial commit

---
+++ src/main/java/piglin/swapswap/SwapswapApplication.java
@@ -0,0 +1,13 @@
+package piglin.swapswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapswapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/piglin/swapswap/SwapswapApplicationTests.java
@@ -0,0 +1,13 @@
+package piglin.swapswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
