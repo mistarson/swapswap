9372b0ee54404bbf5e19a6f02937eadeeecd26a3
wonowonow <wonow_@naver.com>
2024-01-09 00:39:19 +0900

    feat: 글 업로드 기능 html 추가 / enum valid 어노테이션 삭제

--- src/main/java/piglin/swapswap/domain/post/controller/PostController.java
+++ src/main/java/piglin/swapswap/domain/post/controller/PostController.java
@@ -3,7 +3,9 @@ package piglin.swapswap.domain.post.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,16 +19,23 @@ public class PostController {
 
     private final PostService postService;
 
-    @PostMapping
+    @PostMapping("/write")
     public String createPost(@Valid @ModelAttribute PostCreateRequestDto requestDto,
             BindingResult bindingResult
-            , @AuthenticationPrincipal UserDetailsImpl userDetails
+//            , @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
 
         if (bindingResult.hasErrors()) {
-            return "redirect:/posts/write";
+            System.out.println(bindingResult.getAllErrors());
         }
+        Long memberId = 1L;
 
-        return "redirect:/posts/" + postService.createPost(userDetails.getUser().getId(), requestDto).toString();
+        return "redirect:/posts/" + postService.createPost(memberId, requestDto).toString();
+    }
+
+    @GetMapping
+    public String test(Model model) {
+        model.addAttribute("PostCreateRequestDto", new PostCreateRequestDto(null, null, null, null));
+        return "postWrite";
     }
 }
