eb0865ee91e89ab853b1982b80315881ecfed1ff
wonowonow <wonow_@naver.com>
2024-01-09 02:49:52 +0900

    feat: AuthenticationPrincipal 을 이용하여 유저 매개변수 받는 기능 추가

--- src/main/java/piglin/swapswap/domain/post/controller/PostController.java
+++ src/main/java/piglin/swapswap/domain/post/controller/PostController.java
@@ -4,13 +4,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import piglin.swapswap.domain.post.dto.request.PostCreateRequestDto;
 import piglin.swapswap.domain.post.service.PostService;
+import piglin.swapswap.global.security.UserDetailsImpl;
 
 @Controller
 @RequestMapping("/posts")
@@ -20,22 +20,17 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/write")
-    public String createPost(@Valid @ModelAttribute PostCreateRequestDto requestDto,
-            BindingResult bindingResult
-//            , @AuthenticationPrincipal UserDetailsImpl userDetails
-    ) {
+    public String createPost(@Valid @ModelAttribute PostCreateRequestDto requestDto
+            , @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-        if (bindingResult.hasErrors()) {
-            System.out.println(bindingResult.getAllErrors());
-        }
-        Long memberId = 1L;
-
-        return "redirect:/posts/" + postService.createPost(memberId, requestDto).toString();
+        return "redirect:/posts/" + postService.createPost(userDetails.getUserId(), requestDto)
+                .toString();
     }
 
     @GetMapping("/write")
     public String getPostWriteForm(Model model) {
-        model.addAttribute("PostCreateRequestDto", new PostCreateRequestDto(null, null, null, null));
+        model.addAttribute("PostCreateRequestDto",
+                new PostCreateRequestDto(null, null, null, null));
         return "post/postWrite";
     }
 }
