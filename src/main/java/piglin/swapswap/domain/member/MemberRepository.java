230294a7e3dd05da8d256d85a9bef5bf487df40b
ckdnqls <cwp124578@daum.net>
2024-01-12 11:43:45 +0900

    fix : memberRepository 중복 삭제

--- src/main/java/piglin/swapswap/domain/member/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package piglin.swapswap.domain.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import piglin.swapswap.domain.member.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
