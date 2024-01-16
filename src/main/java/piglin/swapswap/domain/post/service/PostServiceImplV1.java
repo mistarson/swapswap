d3ebcc52d02e56b0d889515745cf009ea078101b
wonowonow <wonow_@naver.com>
2024-01-16 11:56:57 +0900

    refactor: 안 쓰는 메서드 삭제

--- src/main/java/piglin/swapswap/domain/post/service/PostServiceImplV1.java
+++ src/main/java/piglin/swapswap/domain/post/service/PostServiceImplV1.java
@@ -2,7 +2,6 @@ package piglin.swapswap.domain.post.service;
 
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import piglin.swapswap.domain.favorite.service.FavoriteService;
 import piglin.swapswap.domain.member.entity.Member;
 import piglin.swapswap.domain.post.constant.Category;
-import piglin.swapswap.domain.post.constant.Category.CategoryName;
 import piglin.swapswap.domain.post.constant.PostConstant;
 import piglin.swapswap.domain.post.dto.request.PostCreateRequestDto;
 import piglin.swapswap.domain.post.dto.request.PostUpdateRequestDto;
@@ -198,23 +196,5 @@ public class PostServiceImplV1 implements PostService {
         if (imageUrlList.size() > PostConstant.IMAGE_MAX_SIZE) {
             throw new BusinessException(ErrorCode.POST_IMAGE_MAX_SIZE);
         }
-    }
-
-    private List<PostGetListResponseDto> getPostListResponseDtoWithFavoriteStatus(Member member,
-            Page<Post> postPage) {
-        List<PostGetListResponseDto> responseDtoList = new ArrayList<>();
-
-        for (Post post : postPage) {
-            Long favoriteCnt = favoriteService.getPostFavoriteCnt(post);
-            boolean favoriteStatus = false;
-
-            if (member != null) {
-                favoriteStatus = favoriteService.isFavorite(post, member);
-            }
-
-//            responseDtoList.add(
-//                    PostMapper.postToGetListResponseDto(post, favoriteCnt, favoriteStatus));
-        }
-        return responseDtoList;
     }
 }
