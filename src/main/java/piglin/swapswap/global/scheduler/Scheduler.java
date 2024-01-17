ff4ba9cb1cdf64be1f801bc4668d4f85b3aff283
wonowonow <wonow_@naver.com>
2024-01-17 17:41:18 +0900

    refactor: 개행

--- src/main/java/piglin/swapswap/global/scheduler/Scheduler.java
+++ src/main/java/piglin/swapswap/global/scheduler/Scheduler.java
@@ -27,6 +27,7 @@ public class Scheduler {
 
     @Scheduled(cron = "0 0 0 * * *")
     public void deleteExpiredMember() {
+
         LocalDateTime fourTeenDaysAgo = LocalDateTime.now().minusDays(14);
 
         List<Member> memberListToDelete = memberRepository.findByIsDeletedIsTrueAndModifiedTimeBefore(fourTeenDaysAgo);
