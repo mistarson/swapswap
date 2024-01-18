c29decc6de716837b3bd5d713be515ed9004593e
ckdnqls <cwp124578@daum.net>
2024-01-18 18:37:06 +0900

    Feature: DealStatus 변경사항 적용

--- src/main/java/piglin/swapswap/domain/deal/constant/DealStatus.java
+++ src/main/java/piglin/swapswap/domain/deal/constant/DealStatus.java
@@ -1,5 +1,15 @@
 package piglin.swapswap.domain.deal.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum DealStatus {
-    REQUESTED, DEALING, COMPLTED
+    REQUESTED("요청 상태"), DEALING("거래 중"), COMPLTIED("거래 완료");
+
+    private final String name;
+
+    DealStatus(String name) {
+        this.name = name;
+    }
 }
+
