3a66d592178b2799962a8d62b36134266455def2
wonowonow <wonow_@naver.com>
2024-01-18 13:54:42 +0900

    refactor: 게시글 리스트 조회 매핑방식 fields -> constructor / 이에 따른 PostGetListResponseDto record 클래스로 재 수정 / alias 삭제

--- src/main/java/piglin/swapswap/domain/post/repository/PostQueryRepositoryImpl.java
+++ src/main/java/piglin/swapswap/domain/post/repository/PostQueryRepositoryImpl.java
@@ -32,15 +32,15 @@ public class PostQueryRepositoryImpl implements PostQueryRepository {
             Member member, LocalDateTime cursorTime) {
 
         return queryFactory
-                .select(Projections.fields(PostGetListResponseDto.class,
-                        post.id.as("postId"),
-                        post.member.id.as("memberId"),
+                .select(Projections.constructor(PostGetListResponseDto.class,
+                        post.id,
+                        post.member.id,
                         post.title,
                         post.imageUrl,
                         post.modifiedUpTime,
                         post.viewCnt,
-                        favorite.post.count().as("favoriteCnt"),
-                        favoriteStatus(member).as("favoriteStatus")))
+                        favorite.post.count(),
+                        favoriteStatus(member)))
                 .from(post)
                 .where(isNotDeleted(), lessThanCursorTime(cursorTime))
                 .leftJoin(favorite)
@@ -57,15 +57,15 @@ public class PostQueryRepositoryImpl implements PostQueryRepository {
             Member member, LocalDateTime cursorTime) {
 
         return queryFactory.select(post)
-                .select(Projections.fields(PostGetListResponseDto.class,
-                        post.id.as("postId"),
-                        post.member.id.as("memberId"),
+                .select(Projections.constructor(PostGetListResponseDto.class,
+                        post.id,
+                        post.member.id,
                         post.title,
                         post.imageUrl,
                         post.modifiedUpTime,
                         post.viewCnt,
-                        favorite.post.count().as("favoriteCnt"),
-                        favoriteStatus(member).as("favoriteStatus")))
+                        favorite.post.count(),
+                        favoriteStatus(member)))
                 .from(post)
                 .where(titleContains(titleCond), categoryEq(categoryCond), isNotDeleted(),
                         lessThanCursorTime(cursorTime))
