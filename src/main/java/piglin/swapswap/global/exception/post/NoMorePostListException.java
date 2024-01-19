84ed48b9ee88a2f3f32e233f2729c771f60d8591
wonowonow <wonow_@naver.com>
2024-01-19 13:51:17 +0900

    refactor: NoMorePostListException BusinessException 상속

--- src/main/java/piglin/swapswap/global/exception/post/NoMorePostListException.java
+++ src/main/java/piglin/swapswap/global/exception/post/NoMorePostListException.java
@@ -1,8 +1,11 @@
 package piglin.swapswap.global.exception.post;
 
-public class NoMorePostListException extends RuntimeException {
+import piglin.swapswap.global.exception.common.BusinessException;
+import piglin.swapswap.global.exception.common.ErrorCode;
+
+public class NoMorePostListException extends BusinessException {
 
     public NoMorePostListException() {
-        super();
+        super(ErrorCode.NO_MORE_POST_LIST);
     }
 }
