8b05e08cf40cac8e2f5c82d0365f4f8a8645e3c5
wonowonow <wonow_@naver.com>
2024-01-19 23:02:18 +0900

    feat: 게시글 목록 조회 시 지역 반환 기능 추가

--- src/main/java/piglin/swapswap/domain/post/dto/response/PostGetListResponseDto.java
+++ src/main/java/piglin/swapswap/domain/post/dto/response/PostGetListResponseDto.java
@@ -2,11 +2,14 @@ package piglin.swapswap.domain.post.dto.response;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import piglin.swapswap.domain.post.constant.City;
 
 public record PostGetListResponseDto(
         Long postId,
 
         Long memberId,
+
+        City city,
 
         String title,
 
