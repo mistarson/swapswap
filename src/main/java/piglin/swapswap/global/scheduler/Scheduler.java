c19a910899d20d41016426e4928391d48f25432e
손창현 (Son Chang Hyun) <cson90563@gmail.com>
2024-01-25 04:03:07 +0900

    test: Scheduler

--- src/main/java/piglin/swapswap/global/scheduler/Scheduler.java
+++ src/main/java/piglin/swapswap/global/scheduler/Scheduler.java
@@ -32,7 +32,7 @@ public class Scheduler {
     private final ApplicationEventPublisher applicationEventPublisher;
 
     @Transactional
-    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 5 4 * * *")
     public void deleteExpiredWalletHistory() {
 
         LocalDateTime fourteenDaysAgo = LocalDateTime.now().minusDays(14);
