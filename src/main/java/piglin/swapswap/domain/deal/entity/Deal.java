f8a26e3bad0c6da3b9b337c7465312733387318b
ckdnqls <cwp124578@daum.net>
2024-01-24 23:53:04 +0900

    fix: 필요없는 import 제거

--- src/main/java/piglin/swapswap/domain/deal/entity/Deal.java
+++ src/main/java/piglin/swapswap/domain/deal/entity/Deal.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.cglib.core.Local;
 import piglin.swapswap.domain.common.BaseTime;
 
 @Entity
