e10be46c9c2dde6a0d0c7e9323461524d7e5dc0b
ckdnqls <cwp124578@daum.net>
2024-01-27 01:36:23 +0900

    feat: 거래 요청시 아무 게시물 선택 안할 시 에러 코드 발생 기능 추가

--- src/main/java/piglin/swapswap/domain/deal/controller/DealController.java
+++ src/main/java/piglin/swapswap/domain/deal/controller/DealController.java
@@ -6,17 +6,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import piglin.swapswap.domain.deal.dto.request.DealCreateRequestDto;
+import piglin.swapswap.domain.deal.dto.response.DealDetailResponseDto;
 import piglin.swapswap.domain.deal.service.DealService;
 import piglin.swapswap.domain.member.entity.Member;
 import piglin.swapswap.global.annotation.AuthMember;
 import piglin.swapswap.global.exception.common.BusinessException;
 import piglin.swapswap.global.exception.common.ErrorCode;
+import piglin.swapswap.global.exception.deal.InvalidDealRequestException;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,16 +27,6 @@ import piglin.swapswap.global.exception.common.ErrorCode;
 public class DealController {
 
     private final DealService dealService;
-
-    @ResponseBody
-    @PostMapping
-        public ResponseEntity<?> createDeal(@AuthMember Member member,
-            @Valid @RequestBody DealCreateRequestDto requestDto) {
-
-        Long dealId = dealService.createDeal(member, requestDto);
-
-        return ResponseEntity.ok().body(dealId);
-    }
 
     @GetMapping("/request")
     public String createDealForm(Model model, @AuthMember Member member,
@@ -54,4 +47,55 @@ public class DealController {
         return "deal/dealCreateForm";
     }
 
+    @ResponseBody
+    @PostMapping
+    public ResponseEntity<?> createDeal(@AuthMember Member member,
+            @Valid @RequestBody DealCreateRequestDto requestDto) {
+
+        if (requestDto.firstPostIdList().isEmpty() && requestDto.secondPostIdList().isEmpty()) {
+            throw new InvalidDealRequestException(ErrorCode.BOTH_POST_ID_LIST_EMPTY_EXCEPTION);
+        }
+
+        Long dealId = dealService.createDeal(member, requestDto);
+
+        return ResponseEntity.ok().body(dealId);
+    }
+
+    @GetMapping("/request/list")
+    public String  getRequestDealList(
+            @AuthMember Member member,
+            Model model) {
+
+        model.addAttribute("dealGetListResponseDto", dealService.getMyRequestDealList(member.getId()));
+        model.addAttribute("memberNickname", member.getNickname());
+
+        return "deal/dealRequestDealListForm";
+    }
+
+    @GetMapping("response/list")
+    public String getResponseDealList(
+            @AuthMember Member member,
+            Model model) {
+
+        model.addAttribute("dealGetListResponseDto", dealService.getMyReceiveDealList(member.getId()));
+        model.addAttribute("memberNickname", member.getNickname());
+
+        return "deal/dealResponseDealListForm";
+    }
+
+   /* @GetMapping("/{dealId}")
+    public String getDeal(
+            @AuthMember Member member, @PathVariable Long dealId,
+            Model model) {
+
+        DealDetailResponseDto responseDto = dealService.getDeal(dealId, member);
+
+        model.addAttribute("dealDetailResponseDto", responseDto);
+        model.addAttribute("firstMemberPostList", postService.getPostSimpleInfoListByPostIdList(responseDto.firstPostIdList()));
+        model.addAttribute("secondMemberPostList", postService.getPostSimpleInfoListByPostIdList(responseDto.secondPostIdList()));
+        model.addAttribute("memberId", member.getId());
+
+        return  "deal/dealRequestDeal";
+    }
+*/
 }
